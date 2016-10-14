a4e0ea265b2de658141d1fb63f851a840537e35d
David Luecke <daff@neyeon.com>
2016-10-14 16:13:03 -0700

    3.0.0

---
+++ dist/global/can-view-parser.js
@@ -0,0 +1,446 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var set = function(name, val){
+		var parts = name.split("."),
+			cur = global,
+			i, part, next;
+		for(i = 0; i < parts.length - 1; i++) {
+			part = parts[i];
+			next = cur[part];
+			if(!next) {
+				next = cur[part] = {};
+			}
+			cur = next;
+		}
+		part = parts[parts.length - 1];
+		cur[part] = val;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		result = module && module.exports ? module.exports : result;
+		modules[moduleName] = result;
+
+		// Set global exports
+		var globalExport = exports[moduleName];
+		if(globalExport && !get(globalExport)) {
+			set(globalExport, result);
+		}
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({"can-util/namespace":"can"},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-util@3.0.1#namespace*/
+define('can-util/namespace', function (require, exports, module) {
+    module.exports = {};
+});
+/*can-util@3.0.1#js/dev/dev*/
+define('can-util/js/dev/dev', function (require, exports, module) {
+});
+/*can-view-parser@3.0.0-pre.7#can-view-parser*/
+define('can-view-parser', function (require, exports, module) {
+    var namespace = require('can-util/namespace'), dev = require('can-util/js/dev/dev');
+    function each(items, callback) {
+        for (var i = 0; i < items.length; i++) {
+            callback(items[i], i);
+        }
+    }
+    function makeMap(str) {
+        var obj = {}, items = str.split(',');
+        each(items, function (name) {
+            obj[name] = true;
+        });
+        return obj;
+    }
+    function handleIntermediate(intermediate, handler) {
+        for (var i = 0, len = intermediate.length; i < len; i++) {
+            var item = intermediate[i];
+            handler[item.tokenType].apply(handler, item.args);
+        }
+        return intermediate;
+    }
+    var alphaNumeric = 'A-Za-z0-9', alphaNumericHU = '-:_' + alphaNumeric, camelCase = /([a-z])([A-Z])/g, stash = '\\{\\{([^\\}]*)\\}\\}\\}?', endTag = new RegExp('^<\\/([' + alphaNumericHU + ']+)[^>]*>'), mustache = new RegExp(stash, 'g'), txtBreak = /<|\{\{/, space = /\s/, alphaRegex = new RegExp('[' + alphaNumeric + ']');
+    var empty = makeMap('area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed');
+    var caseMatters = makeMap('altGlyph,altGlyphDef,altGlyphItem,animateColor,animateMotion,animateTransform,clipPath,feBlend,feColorMatrix,feComponentTransfer,feComposite,feConvolveMatrix,feDiffuseLighting,feDisplacementMap,feDistantLight,feFlood,feFuncA,feFuncB,feFuncG,feFuncR,feGaussianBlur,feImage,feMerge,feMergeNode,feMorphology,feOffset,fePointLight,feSpecularLighting,feSpotLight,feTile,feTurbulence,foreignObject,glyphRef,linearGradient,radialGradient,textPath');
+    var closeSelf = makeMap('colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr');
+    var special = makeMap('script');
+    var tokenTypes = 'start,end,close,attrStart,attrEnd,attrValue,chars,comment,special,done'.split(',');
+    var fn = function () {
+    };
+    var HTMLParser = function (html, handler, returnIntermediate) {
+        if (typeof html === 'object') {
+            return handleIntermediate(html, handler);
+        }
+        var intermediate = [];
+        handler = handler || {};
+        if (returnIntermediate) {
+            each(tokenTypes, function (name) {
+                var callback = handler[name] || fn;
+                handler[name] = function () {
+                    if (callback.apply(this, arguments) !== false) {
+                        intermediate.push({
+                            tokenType: name,
+                            args: [].slice.call(arguments, 0)
+                        });
+                    }
+                };
+            });
+        }
+        function parseStartTag(tag, tagName, rest, unary) {
+            tagName = caseMatters[tagName] ? tagName : tagName.toLowerCase();
+            if (closeSelf[tagName] && stack.last() === tagName) {
+                parseEndTag('', tagName);
+            }
+            unary = empty[tagName] || !!unary;
+            handler.start(tagName, unary);
+            if (!unary) {
+                stack.push(tagName);
+            }
+            HTMLParser.parseAttrs(rest, handler);
+            handler.end(tagName, unary);
+        }
+        function parseEndTag(tag, tagName) {
+            var pos;
+            if (!tagName) {
+                pos = 0;
+            } else {
+                tagName = caseMatters[tagName] ? tagName : tagName.toLowerCase();
+                for (pos = stack.length - 1; pos >= 0; pos--) {
+                    if (stack[pos] === tagName) {
+                        break;
+                    }
+                }
+            }
+            if (pos >= 0) {
+                for (var i = stack.length - 1; i >= pos; i--) {
+                    if (handler.close) {
+                        handler.close(stack[i]);
+                    }
+                }
+                stack.length = pos;
+            }
+        }
+        function parseMustache(mustache, inside) {
+            if (handler.special) {
+                handler.special(inside);
+            }
+        }
+        var callChars = function () {
+            if (charsText) {
+                if (handler.chars) {
+                    handler.chars(charsText);
+                }
+            }
+            charsText = '';
+        };
+        var index, chars, match, stack = [], last = html, charsText = '';
+        stack.last = function () {
+            return this[this.length - 1];
+        };
+        while (html) {
+            chars = true;
+            if (!stack.last() || !special[stack.last()]) {
+                if (html.indexOf('<!--') === 0) {
+                    index = html.indexOf('-->');
+                    if (index >= 0) {
+                        callChars();
+                        if (handler.comment) {
+                            handler.comment(html.substring(4, index));
+                        }
+                        html = html.substring(index + 3);
+                        chars = false;
+                    }
+                } else if (html.indexOf('</') === 0) {
+                    match = html.match(endTag);
+                    if (match) {
+                        callChars();
+                        html = html.substring(match[0].length);
+                        match[0].replace(endTag, parseEndTag);
+                        chars = false;
+                    }
+                } else if (html.indexOf('<') === 0) {
+                    var res = HTMLParser.searchStartTag(html);
+                    if (res) {
+                        callChars();
+                        html = res.html;
+                        parseStartTag.apply(null, res.match);
+                        chars = false;
+                    }
+                } else if (html.indexOf('{{') === 0) {
+                    match = html.match(mustache);
+                    if (match) {
+                        callChars();
+                        html = html.substring(match[0].length);
+                        match[0].replace(mustache, parseMustache);
+                    }
+                }
+                if (chars) {
+                    index = html.search(txtBreak);
+                    if (index === 0 && html === last) {
+                        charsText += html.charAt(0);
+                        html = html.substr(1);
+                        index = html.search(txtBreak);
+                    }
+                    var text = index < 0 ? html : html.substring(0, index);
+                    html = index < 0 ? '' : html.substring(index);
+                    if (text) {
+                        charsText += text;
+                    }
+                }
+            } else {
+                html = html.replace(new RegExp('([\\s\\S]*?)</' + stack.last() + '[^>]*>'), function (all, text) {
+                    text = text.replace(/<!--([\s\S]*?)-->|<!\[CDATA\[([\s\S]*?)]]>/g, '$1$2');
+                    if (handler.chars) {
+                        handler.chars(text);
+                    }
+                    return '';
+                });
+                parseEndTag('', stack.last());
+            }
+            if (html === last) {
+                throw new Error('Parse Error: ' + html);
+            }
+            last = html;
+        }
+        callChars();
+        parseEndTag();
+        handler.done();
+        return intermediate;
+    };
+    var callAttrStart = function (state, curIndex, handler, rest) {
+        var attrName = rest.substring(typeof state.nameStart === 'number' ? state.nameStart : curIndex, curIndex), newAttrName = attrName, oldAttrName = attrName;
+        if (camelCase.test(attrName)) {
+            newAttrName = attrName.replace(camelCase, camelCaseToSpinalCase);
+            dev.warn('can-view-parser: Found attribute with name: ', oldAttrName, '. Converting to: ', newAttrName);
+        }
+        state.attrStart = newAttrName;
+        handler.attrStart(state.attrStart);
+        state.inName = false;
+    };
+    var callAttrEnd = function (state, curIndex, handler, rest) {
+        if (state.valueStart !== undefined && state.valueStart < curIndex) {
+            handler.attrValue(rest.substring(state.valueStart, curIndex));
+        } else if (!state.inValue) {
+        }
+        handler.attrEnd(state.attrStart);
+        state.attrStart = undefined;
+        state.valueStart = undefined;
+        state.inValue = false;
+        state.inName = false;
+        state.lookingForEq = false;
+        state.inQuote = false;
+        state.lookingForName = true;
+    };
+    var camelCaseToSpinalCase = function (match, lowerCaseChar, upperCaseChar) {
+        return lowerCaseChar + '-' + upperCaseChar.toLowerCase();
+    };
+    HTMLParser.parseAttrs = function (rest, handler) {
+        if (!rest) {
+            return;
+        }
+        var i = 0;
+        var curIndex;
+        var state = {
+            inDoubleCurly: false,
+            inName: false,
+            nameStart: undefined,
+            inValue: false,
+            valueStart: undefined,
+            inQuote: false,
+            attrStart: undefined,
+            lookingForName: true,
+            lookingForValue: false,
+            lookingForEq: false
+        };
+        while (i < rest.length) {
+            curIndex = i;
+            var cur = rest.charAt(i);
+            var next = rest.charAt(i + 1);
+            var nextNext = rest.charAt(i + 2);
+            i++;
+            if (cur === '{' && next === '{') {
+                if (state.inValue && curIndex > state.valueStart) {
+                    handler.attrValue(rest.substring(state.valueStart, curIndex));
+                } else if (state.inName && state.nameStart < curIndex) {
+                    callAttrStart(state, curIndex, handler, rest);
+                    callAttrEnd(state, curIndex, handler, rest);
+                } else if (state.lookingForValue) {
+                    state.inValue = true;
+                } else if (state.lookingForEq && state.attrStart) {
+                    callAttrEnd(state, curIndex, handler, rest);
+                }
+                state.inDoubleCurly = true;
+                state.doubleCurlyStart = curIndex + 2;
+                i++;
+            } else if (state.inDoubleCurly) {
+                if (cur === '}' && next === '}') {
+                    var isTriple = nextNext === '}' ? 1 : 0;
+                    handler.special(rest.substring(state.doubleCurlyStart, curIndex));
+                    state.inDoubleCurly = false;
+                    if (state.inValue) {
+                        state.valueStart = curIndex + 2 + isTriple;
+                    }
+                    i += 1 + isTriple;
+                }
+            } else if (state.inValue) {
+                if (state.inQuote) {
+                    if (cur === state.inQuote) {
+                        callAttrEnd(state, curIndex, handler, rest);
+                    }
+                } else if (space.test(cur)) {
+                    callAttrEnd(state, curIndex, handler, rest);
+                }
+            } else if (cur === '=' && (state.lookingForEq || state.lookingForName || state.inName)) {
+                if (!state.attrStart) {
+                    callAttrStart(state, curIndex, handler, rest);
+                    if (i === rest.length) {
+                        callAttrEnd(state, curIndex, handler, rest);
+                    }
+                }
+                state.lookingForValue = true;
+                state.lookingForEq = false;
+                state.lookingForName = false;
+            } else if (state.inName) {
+                if (space.test(cur)) {
+                    callAttrStart(state, curIndex, handler, rest);
+                    state.lookingForEq = true;
+                }
+            } else if (state.lookingForName) {
+                if (!space.test(cur)) {
+                    if (state.attrStart) {
+                        callAttrEnd(state, curIndex, handler, rest);
+                    }
+                    state.nameStart = curIndex;
+                    state.inName = true;
+                }
+            } else if (state.lookingForValue) {
+                if (!space.test(cur)) {
+                    state.lookingForValue = false;
+                    state.inValue = true;
+                    if (cur === '\'' || cur === '"') {
+                        state.inQuote = cur;
+                        state.valueStart = curIndex + 1;
+                    } else {
+                        state.valueStart = curIndex;
+                    }
+                } else if (i === rest.length) {
+                    callAttrEnd(state, curIndex, handler, rest);
+                }
+            }
+        }
+        if (state.inName) {
+            callAttrStart(state, curIndex + 1, handler, rest);
+            callAttrEnd(state, curIndex + 1, handler, rest);
+        } else if (state.lookingForEq) {
+            callAttrEnd(state, curIndex + 1, handler, rest);
+        } else if (state.inValue) {
+            callAttrEnd(state, curIndex + 1, handler, rest);
+        }
+    };
+    HTMLParser.searchStartTag = function (html) {
+        var closingIndex = html.indexOf('>');
+        if (closingIndex === -1 || !alphaRegex.test(html[1])) {
+            return null;
+        }
+        var tagName, tagContent, match, rest = '', unary = '';
+        var startTag = html.substring(0, closingIndex + 1);
+        var isUnary = startTag[startTag.length - 2] === '/';
+        var spaceIndex = startTag.search(space);
+        if (isUnary) {
+            unary = '/';
+            tagContent = startTag.substring(1, startTag.length - 2).trim();
+        } else {
+            tagContent = startTag.substring(1, startTag.length - 1).trim();
+        }
+        if (spaceIndex === -1) {
+            tagName = tagContent;
+        } else {
+            spaceIndex--;
+            tagName = tagContent.substring(0, spaceIndex);
+            rest = tagContent.substring(spaceIndex);
+        }
+        match = [
+            startTag,
+            tagName,
+            rest,
+            unary
+        ];
+        return {
+            match: match,
+            html: html.substring(startTag.length)
+        };
+    };
+    module.exports = namespace.HTMLParser = HTMLParser;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
